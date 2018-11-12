4483b1f22624272317587c54f5a9de404e690016
Michel Tank <micheltank@outlook.com>
2018-11-12 19:38:56 -0200

    implemented correlation

---
+++ correlation.py
@@ -0,0 +1,39 @@
+
+import math 
+
+meanX = 0
+meanY = 0
+def correlation(xList, yList):
+  meanX = calcMean(xList)
+  meanY = calcMean(yList)
+  totalUpCalc = 0
+  totalDownCalc1 = 0
+  totalDownCalc2 = 0 
+
+  for idx in range(len(xList)):
+    totalUpCalc = totalUpCalc + ((xList[idx] - meanX) * (yList[idx] - meanY))
+    totalDownCalc1 = totalDownCalc1 + ((xList[idx] - meanX) ** 2)
+    totalDownCalc2 = totalDownCalc2 + ((yList[idx] - meanY) ** 2)
+  
+  total = totalUpCalc/math.sqrt(totalDownCalc1 * totalDownCalc2)
+  print(total)
+
+def calcMean(x):
+  mean = 0
+  for item in x:
+    mean = mean + item
+  mean = mean/len(x)
+  return mean
+
+x1 = [10,8,13,9,11,14,6,4,12,7,5]
+y1 = [8.04,6.95,7.58,8.81,8.33,9.96,7.24,4.26,10.84,4.82,5.68]
+
+x2 = [10,8,13,9,11,14,6,4,12,7,5]
+y2 = [9.14,8.14,8.47,8.77,9.26,8.10,6.13,3.10,9.13,7.26,4.74]
+
+x3 = [8,8,8,8,8,8,8,8,8,8,19]
+y3 = [6.58,5.76,7.71,8.84,8.47,7.04,5.25,5.56,7.91,6.89,12.50]
+
+correlation(x1, y1)
+correlation(x2, y2)
+correlation(x3, y3)
