91fbe4a70b01152529c771757770ec3228e3b00f
unknown <jose.wollinger@gmail.com>
2018-11-15 11:59:50 -0200

    init fase 2

---
+++ multipleregression.py
@@ -0,0 +1,75 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math 
+#pip install scipy
+import scipy.io as scipy
+from mpl_toolkits.mplot3d import Axes3D
+
+
+mat = scipy.loadmat('data/data.mat')
+#Matriz 3x3. colunas:
+#0 = tamanho, 1 = número de quartos, 2 = preço
+data = mat['data']
+print(mat['data'])
+
+#Tamanho e quartos
+x= data[:, 0:3:2] #slice das colunas
+print(x)
+
+#preço
+y = data[:,1]
+print(y)
+
+
+def calc_correlacao_e_regressao(x, y):
+    corre = correlation(x, y)
+    regre = regression(x, y)
+    return corre, regre
+
+
+def regression(x, y):
+  return b0(x, y) + b1(x, y)
+
+def regmultipla(x , y):
+    print(x)
+
+def scatter3d(x, y, z):
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.scatter(x, y, z)
+    plt.show()
+
+def b1(x, y):
+    total = 0
+    divi = 0
+    for i in range(len(x)):
+        total = total + (x[i] - np.mean(x)) * (y[i] -  np.mean(y))
+        divi = divi + np.power(x[i] - np.mean(x), 2)
+    return total / divi
+
+def b0(x , y):
+    return np.mean(y) - b1(x,  y) * np.mean(x)
+
+def correlation(xList, yList):
+  meanX = np.mean(xList)
+  meanY = np.mean(yList)
+  totalUpCalc = 0
+  totalDownCalc1 = 0
+  totalDownCalc2 = 0 
+
+  for idx in range(len(xList)):
+    totalUpCalc = totalUpCalc + ((xList[idx] - meanX) * (yList[idx] - meanY))
+    totalDownCalc1 = totalDownCalc1 + ((xList[idx] - meanX) ** 2)
+    totalDownCalc2 = totalDownCalc2 + ((yList[idx] - meanY) ** 2)
+  
+  return totalUpCalc / math.sqrt(totalDownCalc1 * totalDownCalc2)
+
+
+scatter3d(x[:,0], y, x[:,1])
+
+
+#Correlação tamanho casa e preço
+correlacao, regressao = calc_correlacao_e_regressao(x[:,0], y)
+
+#Correlação número de quartos e preço
+correlacao, regressao = calc_correlacao_e_regressao(x[:,1], y)
